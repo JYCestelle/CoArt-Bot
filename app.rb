e622228d4b9e5c85213ab7705f0b6a0dafb2a033
yichej <yichej@uw.edum>
2020-05-05 00:33:12 -0400

    add museum intro/

--- app.rb
+++ app.rb
@@ -10,7 +10,7 @@ configure :development do
 end
 
 
-
+#https://hooks.slack.com/services/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
 
 # a very basic sinatra setup
 # with just one route 
@@ -97,6 +97,7 @@ get '/sms/incoming' do
 	session["counter"] ||= 1
 	body = params[:Body] || ""
 	sender = params[:From] || ""
+	session[]
   
 	if session["counter"] == 1
 		message = "Thanks for your first message. From #{sender} saying #{body}"
@@ -163,6 +164,7 @@ end
 def determine_response body, sender 
 		#keyword lists
 		greeting_word = ['hey', 'hello', 'hi']
+		greeting_response = {'I am good', "I'm fine.", "I'm pretty good.", 'pretty good.', "It's okay."}
 		who_word = ['who']
 		what_word = ['what', 'help', 'features', 'functions', 'actions']
 		where_word = ['where']
@@ -178,13 +180,19 @@ def determine_response body, sender
 		facts = IO.readlines("facts.txt")
 
 		if check_input body, greeting_word
-			send_sms_to sender, "Hi there, this is CoArt! Really nice to see you here. My purpose is to help you generate ideas and get inspirations from artworks exploration."
+			send_sms_to sender, "Hi🙌🏼, this is CoArt🤖! Really nice to see you here. My purpose is to help you generate ideas and get inspirations from artworks exploration."
 			sleep(1)
 			send_sms_to sender, "How are you?"
 			sleep(3)
 			#send_sms_to sender, "How are you?"
 			res += "test."
 			# res += "Hi there, this is CoArt! Really nice to see you here. My purpose is to help you generate ideas and get inspirations from artworks exploration."
+		elsif check_input body, greeting_response
+			session['last_intent'] = "museum_intro"
+			res += "Okay, let's start from museum. Do you know the Metropolitan Museum of Art?"
+		elsif session['last_intent'] = "museum_intro"   
+			res += "The Metropolitan Museum of Art of New York City, colloquially "the Met", is the largest art museum in the United States. 
+			With 6,479,548 visitors to its three locations in 2019, it was the fourth most visited art museum in the world."
 		elsif check_input body, who_word
 			res += "It's CoArt Bot created by Estelle Jiang. <br>
 							If you want to know more about me, you can input 'fact' to the Body parameter."
