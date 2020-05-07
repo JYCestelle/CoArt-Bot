976ee38edc12e1a94e7fcb10729d39ada389e8b3
yichej <yichej@uw.edum>
2020-05-06 22:09:36 -0400

    revise the more part.

--- app.rb
+++ app.rb
@@ -212,9 +212,9 @@ def determine_response body, sender
 			res += "Sure, what else you want to explore?"
 			session['last_intent'] = 'begin_explore'
 		elsif check_input body, more_about
-			#info_cont = JSON.parse(session['info_table'])
-			#res += info_cont['artist_url']
-			res += session['info_table']
+			info_cont = JSON.parse(session['info_table'])
+			res += info_cont['title']
+			#res += session['info_table']
 			# if info['artist_url'] != ''
 			# 	res += "I knew! It's a really good one. You can go to " + info['artist_url'] + "to take a closer look at this artist. Also, please check out " + info['met_url']
 			# else 
